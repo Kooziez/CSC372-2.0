aea444f649cc32d27b0de64c52b06696ac1d26e9
Cody Ihle <codyihle@gmail.com>
2025-11-16 16:07:05 -0600

    Intiial upload of Critical Thinking #6

---
+++ StudentSort.java
@@ -0,0 +1,27 @@
+package criticalThinking6;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class StudentSort {
+
+    // Generic Selection Sort
+    public static <T> void selectionSort(List<T> list, Comparator<T> comp) {
+        int n = list.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (comp.compare(list.get(j), list.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            T temp = list.get(i);
+            list.set(i, list.get(minIndex));
+            list.set(minIndex, temp);
+        }
+    }
+}
